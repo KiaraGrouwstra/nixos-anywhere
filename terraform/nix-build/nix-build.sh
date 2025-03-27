63a99d42a9df33dd64bba431454976b17e4c1d21
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2025-03-27 18:36:20 +0100

    allow special_args for non-flake configs, closes #462

--- terraform/nix-build/nix-build.sh
+++ terraform/nix-build/nix-build.sh
@@ -19,50 +19,54 @@ if [[ ${special_args-} == "{}" ]]; then
   fi
 else
   if [[ ${file-} != 'null' ]]; then
-    echo "special_args are currently only supported when using flakes!" >&2
-    exit 1
-  fi
-  # pass the args in a pure fashion by extending the original config
-  rest="$(echo "${attribute}" | cut -d "#" -f 2)"
-  # e.g. config_path=nixosConfigurations.aarch64-linux.myconfig
-  config_path="${rest%.config.*}"
-  # e.g. config_attribute=config.system.build.toplevel
-  config_attribute="config.${rest#*.config.}"
-
-  # grab flake nar from error message
-  flake_rel="$(echo "${attribute}" | cut -d "#" -f 1)"
-  # e.g. flake_rel="."
-  flake_dir="$(readlink -f "${flake_rel}")"
-  if [[ "$flake_dir" =~ ^(\/nix\/store\/[^/]+) ]]; then
-    # for paths in the nix store, take the root path
-    flake_path="${BASH_REMATCH[1]}"
+    base_config="(import ${file})"
+    # e.g. config_attribute=config.system.build.toplevel
+    config_attribute="${attribute}"
   else
-    # for other paths, `builtins.getFlake` will use git
-    flake_path="${flake_dir}"
-  fi
-  flake_json="$(nix flake prefetch "${flake_path}" --json)"
-  flake_nar="$(echo "$flake_json" | jq -r '.hash')"
-  store_path="$(echo "${flake_json}" | jq -r '.storePath')"
-  # while we have a store path now, for a repo this reflects its root level,
-  # so search for the largest child segment yielding a match in that store dir.
-  iter_path="${flake_dir}"
-  
-  while [[ ${iter_path} != "/" ]]; do
-    # we recurse from the specified directory up to the root
-    parent="$(dirname "${iter_path}")"
-    child_segment="${flake_dir//$parent/}/flake.nix"
-    # check if segment exists
-    if [[ -f "${store_path}${child_segment}" ]]; then
-      # if so, record it as our best match
-      target_segment="${child_segment}"
+    # pass the args in a pure fashion by extending the original config
+    rest="$(echo "${attribute}" | cut -d "#" -f 2)"
+    # e.g. config_path=nixosConfigurations.aarch64-linux.myconfig
+    config_path="${rest%.config.*}"
+    # e.g. config_attribute=config.system.build.toplevel
+    config_attribute="config.${rest#*.config.}"
+
+    # grab flake nar from error message
+    flake_rel="$(echo "${attribute}" | cut -d "#" -f 1)"
+    # e.g. flake_rel="."
+    flake_dir="$(readlink -f "${flake_rel}")"
+    if [[ "$flake_dir" =~ ^(\/nix\/store\/[^/]+) ]]; then
+      # for paths in the nix store, take the root path
+      flake_path="${BASH_REMATCH[1]}"
+    else
+      # for other paths, `builtins.getFlake` will use git
+      flake_path="${flake_dir}"
     fi
-    # continue our recursion
-    iter_path="${parent}"
-  done
-  # substitute variables into the template
-  nix_expr="(builtins.getFlake ''file://${flake_dir}?dir=${target_segment//\/flake.nix/}&narHash=${flake_nar}'').${config_path}.extendModules { specialArgs = builtins.fromJSON ''${special_args}''; }"
+    flake_json="$(nix flake prefetch "${flake_path}" --json)"
+    flake_nar="$(echo "$flake_json" | jq -r '.hash')"
+    store_path="$(echo "${flake_json}" | jq -r '.storePath')"
+    # while we have a store path now, for a repo this reflects its root level,
+    # so search for the largest child segment yielding a match in that store dir.
+    iter_path="${flake_dir}"
+  
+    while [[ ${iter_path} != "/" ]]; do
+      # we recurse from the specified directory up to the root
+      parent="$(dirname "${iter_path}")"
+      child_segment="${flake_dir//$parent/}/flake.nix"
+      # check if segment exists
+      if [[ -f "${store_path}${child_segment}" ]]; then
+        # if so, record it as our best match
+        target_segment="${child_segment}"
+      fi
+      # continue our recursion
+      iter_path="${parent}"
+    done
+    # substitute variables into the template
+    base_config="(builtins.getFlake ''file://${flake_dir}?dir=${target_segment//\/flake.nix/}&narHash=${flake_nar}'').${config_path}"
+  fi
   # inject `special_args` into nixos config's `specialArgs`
+  nix_expr="${base_config}.extendModules { specialArgs = builtins.fromJSON ''${special_args}''; }"
+  # FIXME: how can I avoid impure?
   # shellcheck disable=SC2086
-  out=$(nix build --no-link --json ${options} --expr "${nix_expr}" "${config_attribute}")
+  out=$(nix build --impure --no-link --json ${options} --expr "${nix_expr}" "${config_attribute}")
 fi
 printf '%s' "$out" | jq -c '.[].outputs'
