e85296a0ed1232e20e247ad158bb775a9077b639
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2025-04-05 17:08:16 +0200

    terraform: force nix-build to always re-calculate, as terraform would not otherwise see nix config changes

--- terraform/nix-build/main.tf
+++ terraform/nix-build/main.tf
@@ -1,9 +1,21 @@
+resource "null_resource" "always_run" {
+  triggers = {
+    timestamp = "${timestamp()}"
+  }
+}
+
 locals {
   nix_options = jsonencode({
     options = { for k, v in var.nix_options : k => v }
   })
 }
+
 data "external" "nix-build" {
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.always_run
+    ]
+  }
   program = [ "${path.module}/nix-build.sh" ]
   query = {
     attribute = var.attribute
